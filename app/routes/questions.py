5fe8767ee11fd062e1c284b381bbd24a045c6090
Rafael Santos <rafaelnsantos@users.noreply.github.com>
2023-04-25 22:22:25 +0000

    fix questions

--- app/routes/questions.py
+++ app/routes/questions.py
@@ -5,14 +5,13 @@ from app.extensions import db
 
 @bp.route('/', methods=('GET', 'POST'))
 def index():
-    questions = Question.query.all()
-
     if request.method == 'POST':
         new_question = Question(content=request.form['content'],
                                 answer=request.form['answer'])
         db.session.add(new_question)
         db.session.commit()
-        return redirect(url_for('questions.index'))
+        return new_question
 
+    questions = Question.query.all()
     return questions
 
