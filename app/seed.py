f4d3f34dbf0a456ac447b2c317a146fb06924519
Rafael Santos <rafaelnsantos@users.noreply.github.com>
2023-04-26 21:42:47 +0000

    add question to seed

--- app/seed.py
+++ app/seed.py
@@ -1,7 +1,10 @@
 from app.models.post import Post
+from app.models.question import Question
 from app.extensions import db
 
 def seed():
     new_post = Post(content = "content", title = "title")
+    new_question = Question(content = "content", answer = "answer")
+    db.session.add(new_question)
     db.session.add(new_post)
     db.session.commit()
