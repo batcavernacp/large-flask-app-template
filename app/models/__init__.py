3b6f72986e7d9c31aa9c1f61e8f57195786093aa
Rafael Santos <rafaelnsantos@users.noreply.github.com>
2023-04-26 21:42:30 +0000

    fix serialize datetime

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,8 +7,8 @@ class Model(db.Model):
     __abstract__ = True
     
     id: int = db.Column(db.Integer, primary_key=True)
-    createdAt: datetime = db.Column(db.DateTime, default=datetime.utcnow)
-    updatedAt: datetime = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at: datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at: datetime = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     deleted: bool = db.Column(db.Boolean, default=False)
 
     def __init__(self, **kwargs):
@@ -19,5 +19,8 @@ class Model(db.Model):
         """Serialize the model to a dictionary."""
         serialized_data = {}
         for column in self.__table__.columns:
-            serialized_data[column.name] = getattr(self, column.name)
+            if isinstance(getattr(self, column.name), datetime):
+                serialized_data[column.name] = str(getattr(self, column.name))
+            else:
+                serialized_data[column.name] = getattr(self, column.name)
         return serialized_data
